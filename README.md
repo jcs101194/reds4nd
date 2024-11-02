f1b02a51c31191f5adf7f3ef24ed6ceab527ae7a
jcs101194 <jcs101194@yahoo.com>
2024-11-02 09:58:11 -0500

    iphone bytestream reading functionality

--- README.md
+++ README.md
@@ -16,8 +16,8 @@ Usb Protocol:
 
 ```
 Done -include libusb library
-In Progress -get usb bytestream
--interpret bytestream
+Done -get usb bytestream
+In Progress -interpret bytestream
 -inject fun stuff:
 
 	-make phone execute assembly instruction
@@ -29,9 +29,14 @@ In Progress -get usb bytestream
 
 ```
 
-gcc play.c -lusb-1.0	# I have much to learn about linux :(
+gcc play.c -lusb-1.0				# I have much to learn about linux :(
+gcc play.c -lusb-1.0 -fno-stack-protector 	# In case of stack smashing canary
 ./a.out
 
 
 ```
+
+# References
+
+https://www.keil.com/pack/doc/mw/usb/html/_u_s_b__descriptors.html
 
--- play.c
+++ play.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <libusb-1.0/libusb.h>
+#include <unistd.h>				// sleep()
 
 static libusb_device** list;
 static libusb_device* found = NULL;
-static libusb_context* context = NULL;	// Need to initialize context first before you can do
-					// any usb work...
+static libusb_context* context = NULL;		// Need to initialize context first before you can do
+struct libusb_device_descriptor desc;		// any usb work...
 
-/*
+libusb_device_handle* handle;
+
+const uint16_t APPLE_VENDOR_ID = 0x05ac;
+const uint16_t IPHONE_DEVICE_ID = 0x12a8;	// iPhone 5/5C/5S/6/SE/7/8/X/XR	
+
+
 void print_r(struct libusb_device* device)
 {
-	struct libusb_device dv = &device;
-	printf("bus_number: %u", dv.bus_number);
-
+	libusb_get_device_descriptor(device, &desc);
+	printf("pointer: %p, ", device);
+	//printf("bus_number: %u", desc.bus_number);
+	printf("vendor: %04x, ", desc.idVendor);
+	printf("device: %04x, ", desc.idProduct);
+	printf("manufacturer: %04x\n", desc.iManufacturer);
 }
-*/
+
 
 int main()
 {
 	int result = libusb_init(&context);
-	struct libusb_device_descriptor* desc;
+	int err;
+	int* transferred;
+	libusb_device* desiredDevice = NULL;
+	char* dataStream;
 
 	if (result < 0)
 	{
@@ -27,16 +39,32 @@ int main()
 	}
 
 	ssize_t deviceCount = libusb_get_device_list(context, &list);
-	printf(libusb_strerror(result)); printf("\n");
-	printf("hello juan\n");
-	printf("Also device count is: %zu\n", deviceCount); 
-	//print_r(list[0]);
+	printf("Device count is: %zu\n", deviceCount);
 	for (int i = 0; i < deviceCount; i++)
 	{
-		printf("pointer: %p\n", list[i]);
+		libusb_get_device_descriptor(list[i], &desc);
+		print_r(list[i]);
+		if (desc.idVendor == APPLE_VENDOR_ID)
+		{
+			desiredDevice = list[i];
+			printf("Device found!\n");
+			break;
+		}
 	}
-	//libusb_get_device_descriptor(list[0], desc);
 
+	// Open device and read byte stream
+	err = libusb_open(desiredDevice, &handle);
+	if (err)
+	{
+		printf("Error no: %i", err);
+	}
+
+	for (int i = 0; i < 100; i++)
+	{
+		err = libusb_bulk_transfer(handle, 0x100, dataStream, 0x100, *transferred, 1000);
+		printf("%p\n", dataStream);
+		sleep(1);
+	}
 
 	return 0;
 }
